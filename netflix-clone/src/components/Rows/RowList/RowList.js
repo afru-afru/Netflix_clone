a4029f23c52b69ec2355212cadb95ead6dfee8fb
afru-afru <afrubintmame@gmail.com>
2025-02-20 09:54:30 +0300

    row added

---
+++ netflix-clone/src/components/Rows/RowList/RowList.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './rowlist.css'
+import Row from '../Row/Row'
+import requests from '../../../utils/requests'
+
+
+
+const RowList = () => {
+  return (
+    <>
+    <Row title='NETFLIX ORIGINALS' fetchurl={requests.fetchNetflixOriginals} isLargeRow={true}/>
+    <Row  title="Trending Now" fetchurl={requests.fetchActionMovies}/>
+    <Row title="Top Rated" fetchurl={requests.fetchTopRatedMovies}/>
+    <Row title="Action Movies" fetchurl={requests.fetchActionMovies}/>
+    <Row title="Comedy Movies" fetchurl={requests.fetchComedyMovies}/>
+    <Row title="Horror Movies" fetchurl={requests.fetchHorrorMovies}/>
+    <Row title="Romance Movies" fetchurl={requests.fetchRomanceMovies}/>
+    <Row title="Tv Shows" fetchurl={requests.fetchTvShow}/>
+    <Row title="Documentaries" fetchurl={requests.fetchDocumentaries}/>
+    </>
+  )
+}
+
+export default RowList
